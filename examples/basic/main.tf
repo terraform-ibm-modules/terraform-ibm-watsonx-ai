54d23b8caa9b67b56a337a37f52f5f42c310b5f3
Vipin Kumar <vipinkumar@Vipins-MacBook-Pro.local>
2025-03-14 14:16:45 +0530

    updated renovate pr changes here

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -12,7 +12,7 @@ module "resource_group" {
 
 module "cos" {
   source            = "terraform-ibm-modules/cos/ibm//modules/fscloud"
-  version           = "8.19.5"
+  version           = "8.19.8"
   resource_group_id = module.resource_group.resource_group_id
   cos_instance_name = "${var.prefix}-cos"
   cos_plan          = "standard"
