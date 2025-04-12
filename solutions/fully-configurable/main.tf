e2ce299960eea40b04f69a8ecf0d7347aa40f634
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-04-12 19:04:22 +0000

    fix(deps): update terraform terraform-ibm-modules/kms-all-inclusive/ibm to v4.21.7

--- solutions/fully-configurable/main.tf
+++ solutions/fully-configurable/main.tf
@@ -35,7 +35,7 @@ locals {
 module "kms" {
   count                       = (var.existing_cos_kms_key_crn == null && var.existing_kms_instance_crn != null) ? 1 : 0 # no need to create any KMS resources if passing an existing key
   source                      = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version                     = "4.21.6"
+  version                     = "4.21.7"
   create_key_protect_instance = false
   region                      = local.kms_region
   existing_kms_instance_crn   = var.existing_kms_instance_crn
