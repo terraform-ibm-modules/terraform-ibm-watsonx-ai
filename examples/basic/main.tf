a2fb8739ca08f524107abc961aabc10b38ced648
kierra.searle@ibm.com <kierra.searle@ibm.com>
2025-05-23 12:48:08 -0400

    fix: added plans to basic example to prevent error when testing

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -31,6 +31,7 @@ module "watsonx_ai" {
   resource_tags             = var.resource_tags
   resource_group_id         = module.resource_group.resource_group_id
   project_name              = "${var.prefix}-project-basic"
+  watsonx_ai_studio_plan    = "professional-v1"
   watsonx_ai_runtime_plan   = "v2-standard"
   enable_cos_kms_encryption = false
   cos_instance_crn          = module.cos.cos_instance_crn
