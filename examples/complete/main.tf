8bfa15005ebbf100bdcf72d85d91fe316fc57aef
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-07-06 02:58:53 +0100

    chore(deps): update terraform terraform-ibm-modules/cos/ibm to v10 (#134)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "cos" {
   source            = "terraform-ibm-modules/cos/ibm//modules/fscloud"
-  version           = "9.1.0"
+  version           = "10.1.2"
   resource_group_id = module.resource_group.resource_group_id
   cos_instance_name = "${var.prefix}-cos"
   cos_plan          = "standard"
