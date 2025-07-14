c78bfd73516e19e8fb615876454f9119e5a4ded4
aatreyee257 <mukherjeeaatreyee25@gmail.com>
2025-07-14 15:43:39 +0530

    fixed error

--- solutions/fully-configurable/variables.tf
+++ solutions/fully-configurable/variables.tf
@@ -240,14 +240,10 @@ variable "kms_encryption_enabled" {
   default     = true
 
   validation {
-    condition     = (!var.kms_encryption_enabled || var.existing_kms_instance_crn != null || var.existing_cos_kms_key_crn != null)
-    error_message = "When 'kms_encryption_enabled' is true, provide either 'existing_kms_instance_crn' or 'existing_cos_kms_key_crn'."
+  condition = var.kms_encryption_enabled ? ((var.existing_kms_instance_crn != null && trim(var.existing_kms_instance_crn, " ") != "") || (var.existing_cos_kms_key_crn != null && trim(var.existing_cos_kms_key_crn, " ") != "")) : true
+  error_message = "To enable KMS encryption, provide either 'existing_kms_instance_crn' or 'existing_cos_kms_key_crn'."
   }
 
-  validation {
-    condition     = (var.existing_kms_instance_crn == null && var.existing_cos_kms_key_crn == null) || var.kms_encryption_enabled
-    error_message = "When either 'existing_kms_instance_crn' or 'existing_cos_kms_key_crn' is set, 'kms_encryption_enabled' must be true."
-  }
 }
 
 ##############################################################################################################
