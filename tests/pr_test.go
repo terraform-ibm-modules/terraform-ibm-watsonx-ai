98c8353ae264d9ff4b936c1d01d5490825948502
Khuzaima-Shakeel <Khuzaima.Shakeel@ibm.com>
2025-08-18 20:46:19 +0530

    testing

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -2,19 +2,19 @@
 package test
 
 import (
-	"fmt"
+	// "fmt"
 	"log"
 	"math/rand"
 	"os"
-	"strings"
+	// "strings"
 	"testing"
 
-	"github.com/gruntwork-io/terratest/modules/files"
-	"github.com/gruntwork-io/terratest/modules/logger"
-	"github.com/gruntwork-io/terratest/modules/random"
-	"github.com/gruntwork-io/terratest/modules/terraform"
+	// "github.com/gruntwork-io/terratest/modules/files"
+	// "github.com/gruntwork-io/terratest/modules/logger"
+	// "github.com/gruntwork-io/terratest/modules/random"
+	// "github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
+	// "github.com/stretchr/testify/require"
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/common"
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testhelper"
 )
@@ -23,7 +23,8 @@ import (
 const resourceGroup = "geretain-test-resources"
 const basicExampleDir = "examples/basic"
 const completeExampleDir = "examples/complete"
-const standardSolutionTerraformDir = "solutions/fully-configurable"
+
+// const standardSolutionTerraformDir = "solutions/fully-configurable"
 const yamlLocation = "../common-dev-assets/common-go-assets/common-permanent-resources.yaml"
 
 // Current supported regions for watsonx.ai Studio, Runtime and IBM watsonx platform (dataplatform.ibm.com)
@@ -83,46 +84,46 @@ func setupOptions(t *testing.T, prefix string, dir string) *testhelper.TestOptio
 }
 
 // Provision KMS - Key Protect to use in DA tests
-func setupKMSKeyProtect(t *testing.T, region string, prefix string) *terraform.Options {
-	realTerraformDir := "./resources/kp-cos-instance"
-	tempTerraformDir, _ := files.CopyTerraformFolderToTemp(realTerraformDir, fmt.Sprintf(prefix+"-%s", strings.ToLower(random.UniqueId())))
+// func setupKMSKeyProtect(t *testing.T, region string, prefix string) *terraform.Options {
+// 	realTerraformDir := "./resources/kp-cos-instance"
+// 	tempTerraformDir, _ := files.CopyTerraformFolderToTemp(realTerraformDir, fmt.Sprintf(prefix+"-%s", strings.ToLower(random.UniqueId())))
 
-	checkVariable := "TF_VAR_ibmcloud_api_key"
-	val, present := os.LookupEnv(checkVariable)
-	require.True(t, present, checkVariable+" environment variable not set")
-	require.NotEqual(t, "", val, checkVariable+" environment variable is empty")
+// 	checkVariable := "TF_VAR_ibmcloud_api_key"
+// 	val, present := os.LookupEnv(checkVariable)
+// 	require.True(t, present, checkVariable+" environment variable not set")
+// 	require.NotEqual(t, "", val, checkVariable+" environment variable is empty")
 
-	logger.Log(t, "Tempdir: ", tempTerraformDir)
-	existingTerraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		TerraformDir: tempTerraformDir,
-		Vars: map[string]interface{}{
-			"prefix": prefix,
-			"region": region,
-		},
-		Upgrade: true,
-	})
+// 	logger.Log(t, "Tempdir: ", tempTerraformDir)
+// 	existingTerraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+// 		TerraformDir: tempTerraformDir,
+// 		Vars: map[string]interface{}{
+// 			"prefix": prefix,
+// 			"region": region,
+// 		},
+// 		Upgrade: true,
+// 	})
 
-	terraform.WorkspaceSelectOrNew(t, existingTerraformOptions, prefix)
-	_, existErr := terraform.InitAndApplyE(t, existingTerraformOptions)
-	require.NoError(t, existErr, "Init and Apply of temp resources (KP Instance and Key creation) failed")
+// 	terraform.WorkspaceSelectOrNew(t, existingTerraformOptions, prefix)
+// 	_, existErr := terraform.InitAndApplyE(t, existingTerraformOptions)
+// 	require.NoError(t, existErr, "Init and Apply of temp resources (KP Instance and Key creation) failed")
 
-	return existingTerraformOptions
-}
+// 	return existingTerraformOptions
+// }
 
-// Cleanup the resources when KMS encryption key is created.
-func cleanupResources(t *testing.T, terraformOptions *terraform.Options, prefix string) {
-	// Check if "DO_NOT_DESTROY_ON_FAILURE" is set
-	envVal, _ := os.LookupEnv("DO_NOT_DESTROY_ON_FAILURE")
-	// Destroy the temporary existing resources if required
-	if t.Failed() && strings.ToLower(envVal) == "true" {
-		fmt.Println("Terratest failed. Debug the test and delete resources manually.")
-	} else {
-		logger.Log(t, "START: Destroy (existing resources)")
-		terraform.Destroy(t, terraformOptions)
-		terraform.WorkspaceDelete(t, terraformOptions, prefix)
-		logger.Log(t, "END: Destroy (existing resources)")
-	}
-}
+// // Cleanup the resources when KMS encryption key is created.
+// func cleanupResources(t *testing.T, terraformOptions *terraform.Options, prefix string) {
+// 	// Check if "DO_NOT_DESTROY_ON_FAILURE" is set
+// 	envVal, _ := os.LookupEnv("DO_NOT_DESTROY_ON_FAILURE")
+// 	// Destroy the temporary existing resources if required
+// 	if t.Failed() && strings.ToLower(envVal) == "true" {
+// 		fmt.Println("Terratest failed. Debug the test and delete resources manually.")
+// 	} else {
+// 		logger.Log(t, "START: Destroy (existing resources)")
+// 		terraform.Destroy(t, terraformOptions)
+// 		terraform.WorkspaceDelete(t, terraformOptions, prefix)
+// 		logger.Log(t, "END: Destroy (existing resources)")
+// 	}
+// }
 
 func TestRunBasicExample(t *testing.T) {
 
@@ -143,94 +144,94 @@ func TestRunCompleteExample(t *testing.T) {
 }
 
 // Test the DA
-func TestRunStandardSolution(t *testing.T) {
+// func TestRunStandardSolution(t *testing.T) {
 
-	var region = validRegions[rand.Intn(len(validRegions))]
-	prefixExistingRes := fmt.Sprintf("wxai-da-%s", strings.ToLower(random.UniqueId()))
-	existingTerraformOptions := setupKMSKeyProtect(t, region, prefixExistingRes)
+// 	var region = validRegions[rand.Intn(len(validRegions))]
+// 	prefixExistingRes := fmt.Sprintf("wxai-da-%s", strings.ToLower(random.UniqueId()))
+// 	existingTerraformOptions := setupKMSKeyProtect(t, region, prefixExistingRes)
 
-	// Deploy watsonx.ai DA using existing KP details
-	options := testhelper.TestOptionsDefault(&testhelper.TestOptions{
-		Testing:       t,
-		TerraformDir:  standardSolutionTerraformDir,
-		Prefix:        "wxai",
-		Region:        region,
-		ResourceGroup: resourceGroup,
-		IgnoreDestroys: testhelper.Exemptions{ // Ignore for consistency check
-			List: []string{
-				"module.watsonx_ai.module.configure_user.null_resource.configure_user",
-				"module.watsonx_ai.module.configure_user.null_resource.restrict_access",
-			},
-		},
-		IgnoreUpdates: testhelper.Exemptions{ // Ignore for consistency check
-			List: []string{
-				"module.watsonx_ai.module.configure_user.null_resource.configure_user",
-				"module.watsonx_ai.module.configure_user.null_resource.restrict_access",
-			},
-		},
-	})
-	options.TerraformVars = map[string]interface{}{
-		"prefix":                       options.Prefix,
-		"region":                       options.Region,
-		"existing_resource_group_name": resourceGroup,
-		"provider_visibility":          "public",
-		"watsonx_ai_project_name":      "wxai-da-prj",
-		"existing_kms_instance_crn":    terraform.Output(t, existingTerraformOptions, "key_protect_crn"),
-		"kms_endpoint_type":            "public",
-		"existing_cos_instance_crn":    terraform.Output(t, existingTerraformOptions, "cos_crn"),
-		"enable_cos_kms_encryption":    true,
-	}
+// 	// Deploy watsonx.ai DA using existing KP details
+// 	options := testhelper.TestOptionsDefault(&testhelper.TestOptions{
+// 		Testing:       t,
+// 		TerraformDir:  standardSolutionTerraformDir,
+// 		Prefix:        "wxai",
+// 		Region:        region,
+// 		ResourceGroup: resourceGroup,
+// 		IgnoreDestroys: testhelper.Exemptions{ // Ignore for consistency check
+// 			List: []string{
+// 				"module.watsonx_ai.module.configure_user.null_resource.configure_user",
+// 				"module.watsonx_ai.module.configure_user.null_resource.restrict_access",
+// 			},
+// 		},
+// 		IgnoreUpdates: testhelper.Exemptions{ // Ignore for consistency check
+// 			List: []string{
+// 				"module.watsonx_ai.module.configure_user.null_resource.configure_user",
+// 				"module.watsonx_ai.module.configure_user.null_resource.restrict_access",
+// 			},
+// 		},
+// 	})
+// 	options.TerraformVars = map[string]interface{}{
+// 		"prefix":                       options.Prefix,
+// 		"region":                       options.Region,
+// 		"existing_resource_group_name": resourceGroup,
+// 		"provider_visibility":          "public",
+// 		"watsonx_ai_project_name":      "wxai-da-prj",
+// 		"existing_kms_instance_crn":    terraform.Output(t, existingTerraformOptions, "key_protect_crn"),
+// 		"kms_endpoint_type":            "public",
+// 		"existing_cos_instance_crn":    terraform.Output(t, existingTerraformOptions, "cos_crn"),
+// 		"enable_cos_kms_encryption":    true,
+// 	}
 
-	output, err := options.RunTestConsistency()
-	assert.Nil(t, err, "This should not have errored")
-	assert.NotNil(t, output, "Expected some output")
+// 	output, err := options.RunTestConsistency()
+// 	assert.Nil(t, err, "This should not have errored")
+// 	assert.NotNil(t, output, "Expected some output")
 
-	cleanupResources(t, existingTerraformOptions, prefixExistingRes)
-}
+// 	cleanupResources(t, existingTerraformOptions, prefixExistingRes)
+// }
 
-func TestRunStandardUpgradeSolution(t *testing.T) {
+// func TestRunStandardUpgradeSolution(t *testing.T) {
 
-	var region = validRegions[rand.Intn(len(validRegions))]
-	prefixExistingRes := fmt.Sprintf("wxai-da-%s", strings.ToLower(random.UniqueId()))
-	existingTerraformOptions := setupKMSKeyProtect(t, region, prefixExistingRes)
+// 	var region = validRegions[rand.Intn(len(validRegions))]
+// 	prefixExistingRes := fmt.Sprintf("wxai-da-%s", strings.ToLower(random.UniqueId()))
+// 	existingTerraformOptions := setupKMSKeyProtect(t, region, prefixExistingRes)
 
-	// Deploy watsonx.ai DA using existing KP details
-	options := testhelper.TestOptionsDefault(&testhelper.TestOptions{
-		Testing:       t,
-		TerraformDir:  standardSolutionTerraformDir,
-		Prefix:        "wxai-upg",
-		Region:        region,
-		ResourceGroup: resourceGroup,
-		IgnoreDestroys: testhelper.Exemptions{ // Ignore for consistency check
-			List: []string{
-				"module.watsonx_ai.module.configure_user.null_resource.configure_user",
-				"module.watsonx_ai.module.configure_user.null_resource.restrict_access",
-			},
-		},
-		IgnoreUpdates: testhelper.Exemptions{ // Ignore for consistency check
-			List: []string{
-				"module.watsonx_ai.module.configure_user.null_resource.configure_user",
-				"module.watsonx_ai.module.configure_user.null_resource.restrict_access",
-			},
-		},
-	})
-	options.TerraformVars = map[string]interface{}{
-		"prefix":                       options.Prefix,
-		"region":                       options.Region,
-		"existing_resource_group_name": resourceGroup,
-		"provider_visibility":          "public",
-		"watsonx_ai_project_name":      "wxai-ug-prj",
-		"existing_kms_instance_crn":    terraform.Output(t, existingTerraformOptions, "key_protect_crn"),
-		"kms_endpoint_type":            "public",
-		"existing_cos_instance_crn":    terraform.Output(t, existingTerraformOptions, "cos_crn"),
-		"enable_cos_kms_encryption":    true,
-	}
+// 	// Deploy watsonx.ai DA using existing KP details
+// 	options := testhelper.TestOptionsDefault(&testhelper.TestOptions{
+// 		Testing:       t,
+// 		TerraformDir:  standardSolutionTerraformDir,
+// 		Prefix:        "wxai-upg",
+// 		Region:        region,
+// 		ResourceGroup: resourceGroup,
+// 		IgnoreDestroys: testhelper.Exemptions{ // Ignore for consistency check
+// 			List: []string{
+// 				"module.watsonx_ai.module.configure_user.null_resource.configure_user",
+// 				"module.watsonx_ai.module.configure_user.null_resource.restrict_access",
+// 			},
+// 		},
+// 		IgnoreUpdates: testhelper.Exemptions{ // Ignore for consistency check
+// 			List: []string{
+// 				"module.watsonx_ai.module.configure_user.null_resource.configure_user",
+// 				"module.watsonx_ai.module.configure_user.null_resource.restrict_access",
+// 			},
+// 		},
+// 	})
+// 	options.TerraformVars = map[string]interface{}{
+// 		"prefix":                       options.Prefix,
+// 		"region":                       options.Region,
+// 		"existing_resource_group_name": resourceGroup,
+// 		"provider_visibility":          "public",
+// 		"watsonx_ai_project_name":      "wxai-ug-prj",
+// 		"existing_kms_instance_crn":    terraform.Output(t, existingTerraformOptions, "key_protect_crn"),
+// 		"kms_endpoint_type":            "public",
+// 		"existing_cos_instance_crn":    terraform.Output(t, existingTerraformOptions, "cos_crn"),
+// 		"enable_cos_kms_encryption":    true,
+// 	}
 
-	output, err := options.RunTestUpgrade()
-	if !options.UpgradeTestSkipped {
-		assert.Nil(t, err, "This should not have errored")
-		assert.NotNil(t, output, "Expected some output")
-	}
+// 	output, err := options.RunTestUpgrade()
+// 	if !options.UpgradeTestSkipped {
+// 		assert.Nil(t, err, "This should not have errored")
+// 		assert.NotNil(t, output, "Expected some output")
+// 	}
 
-	cleanupResources(t, existingTerraformOptions, prefixExistingRes)
-}
+// 	cleanupResources(t, existingTerraformOptions, prefixExistingRes)
+// }
