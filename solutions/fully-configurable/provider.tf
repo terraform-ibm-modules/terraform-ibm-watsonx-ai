0e42eab404bb9a710f2c3c66f07e6f6f76b7aa02
Khuzaima-Shakeel <Khuzaima.Shakeel@ibm.com>
2025-08-28 15:33:13 +0530

    minor fix

--- solutions/fully-configurable/provider.tf
+++ solutions/fully-configurable/provider.tf
@@ -3,10 +3,9 @@
 ########################################################################################################################
 
 provider "ibm" {
-  ibmcloud_api_key      = var.ibmcloud_api_key
-  region                = var.region
-  visibility            = var.provider_visibility
-  private_endpoint_type = (var.provider_visibility == "private" && var.region == "ca-mon") ? "vpe" : null
+  ibmcloud_api_key = var.ibmcloud_api_key
+  region           = var.region
+  visibility       = var.provider_visibility
 }
 
 provider "ibm" {
