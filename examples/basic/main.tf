6d71bc923ec563a25f837e9b9404195357537abe
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-11-10 00:33:59 +0000

    fix(deps): update dependencies

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -12,7 +12,7 @@ module "resource_group" {
 
 module "cos" {
   source            = "terraform-ibm-modules/cos/ibm//modules/fscloud"
-  version           = "10.5.6"
+  version           = "10.5.8"
   resource_group_id = module.resource_group.resource_group_id
   cos_instance_name = "${var.prefix}-cos"
   cos_plan          = "standard"
