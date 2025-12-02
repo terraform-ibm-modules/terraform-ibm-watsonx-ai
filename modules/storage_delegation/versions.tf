872174d4e08d04742456535e4a0ecbbd35f46be7
Harika-Ponna <Harika.Ponna@ibm.com>
2025-12-02 13:54:43 +0530

    fix : updated the restapi version for modules and added variable to ignore server changes

--- modules/storage_delegation/versions.tf
+++ modules/storage_delegation/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     restapi = {
       source  = "Mastercard/restapi"
-      version = ">= 1.19.1"
+      version = ">= 1.19.1, < 3.0.0"
     }
     ibm = {
       source  = "IBM-Cloud/ibm"
