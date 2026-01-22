7f64c597b790e6c379a15006186d281baeb7584d
Harika-Ponna <Harika.Ponna@ibm.com>
2026-01-22 19:58:40 +0530

    feat: add binary installation support for Waypoint runtime compatibility

--- modules/configure_project/main.tf
+++ modules/configure_project/main.tf
@@ -110,17 +110,29 @@ data "ibm_iam_auth_token" "tokendata" {}
 
 locals {
   sensitive_tokendata = sensitive(data.ibm_iam_auth_token.tokendata.iam_access_token)
+  binaries_path       = "/tmp"
+}
+
+resource "terraform_data" "install_required_binaries" {
+  count = var.install_required_binaries ? 1 : 0
+  triggers_replace = {
+    members = jsonencode(var.watsonx_ai_new_project_members)
+  }
+  provisioner "local-exec" {
+    command     = "${path.module}/modules/configure_user/scripts/install-binaries.sh ${local.binaries_path}"
+    interpreter = ["/bin/bash", "-c"]
+  }
 }
 
 resource "null_resource" "add_collaborators_to_project" {
   for_each   = { for member in var.watsonx_ai_new_project_members : member.email => member }
-  depends_on = [data.ibm_iam_auth_token.tokendata]
+  depends_on = [terraform_data.install_required_binaries, data.ibm_iam_auth_token.tokendata]
   triggers = {
     members = jsonencode(var.watsonx_ai_new_project_members)
   }
 
   provisioner "local-exec" {
-    command     = "${path.module}/scripts/add_collaborators_to_project.sh"
+    command     = "${path.module}/scripts/add_collaborators_to_project.sh ${local.binaries_path}"
     interpreter = ["/bin/bash", "-c"]
     environment = {
       iam_token  = local.sensitive_tokendata
--- modules/configure_user/main.tf
+++ modules/configure_user/main.tf
@@ -2,6 +2,19 @@ data "ibm_iam_auth_token" "tokendata" {}
 
 locals {
   sensitive_tokendata = sensitive(data.ibm_iam_auth_token.tokendata.iam_access_token)
+  binaries_path       = "/tmp"
+
+}
+
+resource "terraform_data" "install_required_binaries" {
+  count = var.install_required_binaries ? 1 : 0
+  triggers_replace = {
+    script_hash = filesha256("${path.module}/scripts/install-binaries.sh")
+  }
+  provisioner "local-exec" {
+    command     = "${path.module}/scripts/install-binaries.sh ${local.binaries_path}"
+    interpreter = ["/bin/bash", "-c"]
+  }
 }
 
 resource "terraform_data" "configure_user" {
@@ -12,7 +25,7 @@ resource "terraform_data" "configure_user" {
   }
 
   provisioner "local-exec" {
-    command     = "${path.module}/scripts/add_user.sh"
+    command     = "${path.module}/scripts/add_user.sh ${local.binaries_path}"
     interpreter = ["/bin/bash", "-c"]
     environment = {
       iam_token         = local.sensitive_tokendata
@@ -29,7 +42,7 @@ resource "terraform_data" "restrict_access" {
   }
 
   provisioner "local-exec" {
-    command     = "${path.module}/scripts/enforce_account_restriction.sh"
+    command     = "${path.module}/scripts/enforce_account_restriction.sh ${local.binaries_path}"
     interpreter = ["/bin/bash", "-c"]
     environment = {
       iam_token = local.sensitive_tokendata
