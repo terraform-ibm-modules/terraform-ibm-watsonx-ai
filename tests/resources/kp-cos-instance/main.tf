d4393cc5a26ef97676f31c1081a5ce9baa642328
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-01-25 16:01:53 +0000

    chore(deps): update terraform terraform-ibm-modules/cos/ibm to v10.9.6

--- tests/resources/kp-cos-instance/main.tf
+++ tests/resources/kp-cos-instance/main.tf
@@ -19,7 +19,7 @@ module "kms" {
 # Temporary workaround for issue https://github.ibm.com/GoldenEye/issues/issues/15533
 module "cos_module" {
   source                     = "terraform-ibm-modules/cos/ibm"
-  version                    = "10.9.5"
+  version                    = "10.9.6"
   resource_group_id          = module.resource_group.resource_group_id
   region                     = var.region
   cos_instance_name          = "${var.prefix}-cos"
