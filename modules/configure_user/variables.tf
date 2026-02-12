e93aff674bb73210e83998b4541bf1dbc1cfa58b
Harika <86517031+HarikaPonna@users.noreply.github.com>
2026-02-12 16:07:00 +0530

    feat: added a script that will install `jq` from the public internet if it is not detected in the terraform runtime. This can be disabled by setting `install_required_binaries` to false however `jq` is required to run this module. (#221)

--- modules/configure_user/variables.tf
+++ modules/configure_user/variables.tf
@@ -7,3 +7,10 @@ variable "region" {
   description = "Region to be used for the watsonx resources creation."
   type        = string
 }
+
+variable "install_required_binaries" {
+  type        = bool
+  default     = true
+  description = "When set to true, a script will run to check if `jq` exist on the runtime and if not attempt to download it from the public internet and install it to /tmp. Set to false to skip running this script."
+  nullable    = false
+}
