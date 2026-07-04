93e2c6f508a7bcc05f58e2e50c63b26be998ad0f
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-07-04 14:43:22 +0000

    chore(deps): update terraform terraform-ibm-modules/cos/ibm to v10.17.2

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "cos" {
   source            = "terraform-ibm-modules/cos/ibm//modules/fscloud"
-  version           = "10.17.1"
+  version           = "10.17.2"
   resource_group_id = module.resource_group.resource_group_id
   cos_instance_name = "${var.prefix}-cos"
   cos_plan          = "standard"
